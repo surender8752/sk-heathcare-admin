27b4b16fb97c03b013292493e68ed96a4ed8400c
surender <surenderthakur40437@gmail.com>
2026-01-11 02:00:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { AdminContext } from './context/AdminContext'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import AllAppointments from './pages/AllAppointments'
+import AddDoctor from './pages/AddDoctor'
+import DoctorsList from './pages/DoctorsList'
+
+const App = () => {
+    const { aToken } = useContext(AdminContext)
+
+    return aToken ? (
+        <div className='bg-[#F8F9FD]'>
+            <ToastContainer />
+            <Navbar />
+            <div className='flex items-start'>
+                <Sidebar />
+                <Routes>
+                    <Route path='/' element={<></>} />
+                    <Route path='/admin-dashboard' element={<Dashboard />} />
+                    <Route path='/all-appointments' element={<AllAppointments />} />
+                    <Route path='/add-doctor' element={<AddDoctor />} />
+                    <Route path='/doctor-list' element={<DoctorsList />} />
+                </Routes>
+            </div>
+        </div>
+    ) : (
+        <>
+            <Login />
+            <ToastContainer />
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+    const { aToken } = useContext(AdminContext)
+
+    return (
+        <div className='min-h-screen bg-white border-r'>
+            {
+                aToken && <ul className='text-[#515151] mt-5'>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin-dashboard'}>
+                        <p>Dashboard</p>
+                    </NavLink>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/all-appointments'}>
+                        <p>Appointments</p>
+                    </NavLink>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/add-doctor'}>
+                        <p>Add Doctor</p>
+                    </NavLink>
+                    <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/doctor-list'}>
+                        <p>Doctors List</p>
+                    </NavLink>
+                </ul>
+            }
+        </div>
+    )
+}
+
+export default Sidebar
